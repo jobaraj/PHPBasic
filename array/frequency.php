5b64e4cb3ac949f4996e6a767045c6f603a548fd
jobaraj <jobaraj1967@gmail.com>
2023-05-14 23:22:10 +0600

    14-05-23

--- array/frequency.php
+++ array/frequency.php
@@ -8,17 +8,46 @@
 		</head>
 		
 		<?php
-			
-			
-			$array = array('Hello');
- 			for($i=0; $i< sizeof($array); $i++){
-				if($array[$i] > $max){
-					$max = $array[$i];
+
+			$array = "Hellow world";
+			$n = strlen($array);
+			$temp = 0;
+			$temp = array();
+		
+			for ($i = 0; $i < $n; $i++) {
+		
+				for ($j = 0; $j < $n; $j++) {
+					if ($array[$i] === $array[$j]) {
+		
+						$temp[$i]++;
+					   
+		
+		
+		
+		
+					}
 				}
 			}
-			echo 'The Frequency of Letter : '.$max;
-		?>
+		
+			for ($i = 0; $i < $n; $i++) {
+				$array[$i];
+				$Dupli = false;
+			
+				
+				for ($j = 0; $j < $i; $j++) {
+					if ($array[$j] === $array[$i]) {
+						$Dupli = true;
+						break;
+					}
+				}
+			
+				
+				if (!$Dupli) {
+					echo $array[$i] .":".$temp[$i]. "<br>";
+				}
+			}
 
+    ?>
 
 	</body>
 </html>
